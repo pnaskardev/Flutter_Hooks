9b25608153a7f622f437c575882f949e164e0356
pnaskardev <pnaskardev@gmail.com>
2023-02-22 22:43:04 +0530

    added useListenable in example 4

--- testing_flutter_hooks/lib/main.dart
+++ testing_flutter_hooks/lib/main.dart
@@ -1,14 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:testing_flutter_hooks/utils/utils.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-const url=
-'https://images.unsplash.com/photo-1677054727286-feefd75880b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80';
+class CountDown extends ValueNotifier<int>
+{
+  late StreamSubscription sub;
+  CountDown({required int from}):super(from)
+  {
+    sub=Stream.periodic
+    (
+      const Duration(seconds: 1),
+      (v)=>from-v
+    ).takeWhile((element) => element>=0)
+    .listen((event) 
+    {
+      value=value;
+    });
+  }
+
+  @override
+  void dispose() 
+  {
+    sub.cancel();
+    super.dispose();
+  }
+}
 
 class MainApp extends HookWidget 
 {
@@ -17,24 +38,8 @@ class MainApp extends HookWidget
   @override
   Widget build(BuildContext context) 
   {
-    // DOES NOT PERSIST STATE 
-    // final image=useFuture
-    // (
-    //   NetworkAssetBundle(Uri.parse(url))
-    //   .load(url)
-    //   .then((data)=>data.buffer.asUint8List())
-    //   .then((data) => Image.memory(data),)
-    // );
-
-    // SAVES THE STATE OF THE IMAGE
-    final future=useMemoized
-    (
-      ()=>NetworkAssetBundle(Uri.parse(url))
-        .load(url)
-        .then((data)=>data.buffer.asUint8List())
-        .then((data) => Image.memory(data),)
-    );
-    final snapshot=useFuture(future);
+    final countDown=useMemoized(()=>CountDown(from: 20));
+    final notifier=useListenable(countDown);
     return  MaterialApp
     (
       home: Scaffold
@@ -44,16 +49,10 @@ class MainApp extends HookWidget
           centerTitle: true,
           title: const Text('HomePage'),
         ),
-        body:  Center
+        body: Center
         (
-          child: Column
-          (
-            children: 
-            [
-              snapshot.data
-            ].compactMap().toList(),
-          ),
-        )
+          child: Text(notifier.value.toString()),
+        ),
       ),
     );
   }
